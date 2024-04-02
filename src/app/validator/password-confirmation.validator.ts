b4762d931c4361d20b80c416e64c503f5209ada9
guro svanidze <svanidzeguro@gmail.com>
2024-04-02 12:27:16 +0400

    job done!

--- src/app/validator/password-confirmation.validator.ts
+++ src/app/validator/password-confirmation.validator.ts
@@ -4,5 +4,8 @@ export const passwordConfirmationValidator = (control: AbstractControl) => {
   let password = control?.get('password');
   let confirm_password = control?.get('confirm_password');
   let doesntMatch = password?.value !== confirm_password?.value;
-  return doesntMatch ? { passwordNotMatch: true } : null;
+  if (doesntMatch) {
+    control?.get('confirm_password')?.setErrors({passwordNotMatch: true});
+  }
+  return doesntMatch ? {passwordNotMatch: true} : null;
 };
